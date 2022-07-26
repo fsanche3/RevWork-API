bd775dd096cb12b4b6108c0f7ba5af6f5fe1a144
New Pagodi <NewPagodi@users.noreply.github.com>
2022-07-26 14:55:29 -0500

    Add files generated by spring initializr

---
+++ src/test/java/com/revature/revwork/RevworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.revwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
