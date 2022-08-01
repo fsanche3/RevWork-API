08f033b481151e6cea5e567f914efb2ba18d468e
New Pagodi <NewPagodi@users.noreply.github.com>
2022-08-01 13:19:41 -0500

    Add login controller

---
+++ src/main/java/p2/revature/revwork/utils/JwtUtil.java
@@ -0,0 +1,46 @@
+package p2.revature.revwork.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+
+
+@Component
+public class JwtUtil {
+
+	private static JWTVerifier jwtVerifier;
+	
+    private JwtUtil() {
+    }
+
+    static {
+    	jwtVerifier = null;
+    }
+	
+	
+    @Bean
+    @Scope("prototype")
+    public JWTCreator.Builder getJwtBuilder() {
+        return JWT.create();
+    }
+	
+    @Bean
+    @Scope("singleton")
+    public JWTVerifier getJwtVerifierr() {
+    	if ( jwtVerifier == null ) {
+    		Algorithm algorithm = Algorithm.HMAC256("michael"); //use more secure key
+    		
+    		jwtVerifier = JWT.require(algorithm)
+			        .withIssuer("auth0")
+			        .build(); //Reusable verifier instance
+    	}
+    	
+    	return jwtVerifier;
+    }
+    
+}
